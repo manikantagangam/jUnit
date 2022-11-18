dd4a770a5788a07d8b1b8fed6fea69035d839cef
mani <mani@localhost>
2022-11-18 09:51:40 +0530

    Added Numbers and srings

---
+++ src/jUnitPackage/jUnitFunctions.java
@@ -0,0 +1,31 @@
+package jUnitPackage;
+
+import java.util.Scanner;
+
+public class jUnitFunctions {
+	static void AddNumbers() {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int ans=a+b;
+		System.out.println(ans);
+	}
+	static void AddStrings() {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		String b = sc.nextLine();
+		String ans=a+b;
+		System.out.println(ans);
+	}
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		if (x==1) {
+			AddNumbers();
+		}
+		else {
+			AddStrings();
+		}
+	}
+}
+
